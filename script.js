4ef82e7ffb316fc8d9279c3298a57dad925e574c
beatrice2303 <137236291+beatrice2303@users.noreply.github.com>
2024-10-02 06:09:44 +0000

    An App that randomly generates numbers

--- script.js
+++ script.js
@@ -1,39 +1,7 @@
-document.addEventListener("DOMContentLoaded",function(){
+const generateBtn = document.getElementById("generate-btn");
+const numberElement = document.getElementById("random-number");
 
-    const numberInput = document.getElementById("numberInput");
-    const checkButton = document.getElementById("checkButton");
-    const resultMessage = document.getElementById("resultMessage");
-
-    checkButton.addEventListener("click",function(){
-
-        const number = parseInt(numberInput.value);
-        
-        if(isNaN(number)){
-            resultMessage.textContent = "Please enter a valid number";
-            resultMessage.style.color = "red";
-            return;
-        }    
-
-        if(isPrime(number)){
-            resultMessage.textContent = `${number} is a prime number`
-            resultMessage.style.color = "green";
-        }else{
-            resultMessage.textContent = `${number} is not a prime number`
-            resultMessage.style.color = "red";
-        }
-
-
-    });
-
-    function isPrime(num){
-
-        if (num <= 1) return false;
-        if (num <=3) return true;
-
-        if(num % 2 === 0 || num % 3 === 0) return false;
-
-        return true;
-    }
-
-
+generateBtn.addEventListener("click",()=>{
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    numberElement.textContent = randomNumber;
 });
